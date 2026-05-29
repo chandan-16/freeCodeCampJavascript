c91c20d4706234bf1b42e26a8276c2eab4915234
chandan-16 <chandangautam420.cg@gmail.com>
2026-05-29 23:14:57 +0530

    Build a loan Qualification Checker using functions and conditional if else statements :)

--- freeCodeCampJavascript.js
+++ freeCodeCampJavascript.js
@@ -72,3 +72,36 @@ console.log(calculateArea(23,120));
 
 const calculateArea2 = (width, height) => width * height;
 console.log(calculateArea2(27,120));
+
+// Build a loan Qualification Checker using functions and conditional if else statements :)
+
+const minIncomeForDuplex = 60000;
+const minCreditScoreForDuplex = 700;
+
+const minIncomeForCondo = 45000;
+const minCreditScoreForCondo = 680;
+
+const minIncomeForCar = 30000;
+const minCreditScoreForCar = 650;
+
+function getLoanMessage(annualIncome, creditScore) {
+  if(creditScore >= minCreditScoreForDuplex && annualIncome >= minIncomeForDuplex) {
+    return "You qualify for a duplex, condo, and car loan."
+  } else if (annualIncome >= minIncomeForCondo && creditScore >= minCreditScoreForCondo) {
+    return "You qualify for a condo and car loan."
+  } else if (annualIncome >= minIncomeForCar && creditScore >= minCreditScoreForCar) {
+    return "You qualify for a car loan."
+  } else {
+    return "You don't qualify for any loans."
+  }
+}
+
+let duplexLoanMsg = getLoanMessage(85000, 850);
+let condoLoanMsg = getLoanMessage(65000, 690);
+let carLoanMsg = getLoanMessage(45000, 660);
+let noLoanMsg = getLoanMessage(25000, 550);
+
+console.log(duplexLoanMsg);
+console.log(condoLoanMsg);
+console.log(carLoanMsg);
+console.log(noLoanMsg);
