e12d01e5fe5a48d904b7e587ef4f4206dfb16caa
chandan-16 <chandangautam420.cg@gmail.com>
2026-06-01 14:17:41 +0530

    Created a new practice sheet to solve the concepts related to freecodecamp js certification

---
+++ practiceSheet1.js
@@ -0,0 +1,15 @@
+// Create variables for name, age, city and print them. 
+
+let name = 'Chandan Gautam';
+const age = 26;
+const city = 'Pune';
+console.log(name, age, city);
+
+// Count the number of characters in a string. 
+
+let stringData = "Chandan Ashok Gautam";
+console.log(stringData.length);
+
+// Convert a string to uppercase 
+
+console.log(stringData.toUpperCase())
